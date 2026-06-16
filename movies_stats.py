582f00651a2178c93d3872e13fbabcb6e7eafc65
mozaican <oana.buturas@gmail.com>
2026-06-16 16:39:54 +0200

    added logic for saving the extracted data

--- movies_stats.py
+++ movies_stats.py
@@ -1,5 +1,7 @@
 import os
+import json
 import requests
+from datetime import date
 from dotenv import load_dotenv
 
 
@@ -25,21 +27,28 @@ def get_movies_stats():
 
         movies_stats = []
         for movie in data.get('results', []):
-            movie_data = {
+            movies_stats.append({
                 "id": movie.get('id'),
                 "title": movie.get('title'),
                 "popularity": movie.get('popularity'),
                 "vote_average": movie.get('vote_average'),
                 "release_date": movie.get('release_date')
-            }
-            movies_stats.append(movie_data)
-            
+            })
+
         return movies_stats
 
     except requests.exceptions.RequestException as e:
         raise e
+    
+
+def save_to_json(exatracted_data):
+    file_path = f"./data/movies_stats_{date.today()}.json"
+
+    with open(file_path, 'w', encoding='utf-8') as json_outfile:
+        json.dump(exatracted_data, json_outfile, indent=4, ensure_ascii=False)
 
 
 if __name__ == "__main__":
-    get_movies_stats()
+    movies_stats = get_movies_stats()
+    save_to_json(movies_stats)
 
